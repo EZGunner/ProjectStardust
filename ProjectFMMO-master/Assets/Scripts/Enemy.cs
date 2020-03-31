1969f40d93d56b17239d2552995a1a6678cc3913
joshuajgit <43454615+joshuajgit@users.noreply.github.com>
2020-03-30 21:15:47 -0400

    beginning the battle system

---
+++ ProjectFMMO-master/Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int HP;
+    public int attack;
+    public int defense;
+
+    public bool notHammerable; //if true, enemy then can't be reached w/ hammer
+    public bool notJumpable; //if true, enemy then can't even be reached w/ jump
+    public bool spikey; //if true, player will take spike damage trying to jump on this enemy
+    public bool firey; //if true, player will be burned trying to jump on this enemy
+
+    public PlayerInBattle targetPlayer; //the player in battle to interact with
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void Attack() {
+        //assuming multiple attack targets, the enemy has to choose one
+
+        //for now, assuming only one target: the player themselves in the battle scene
+        targetPlayer.DamageIncoming();
+    }
+
+    void TakeDamage(int damage) {
+        int incomingDamage = damage - defense;
+        if (incomingDamage < 0) {
+            incomingDamage = 0;
+        }
+        HP -= incomingDamage;
+
+        //then check if HP is 0 or less; if so, enemy dies
+        if (HP <= 0) {
+            Die();
+        }
+    }
+
+    void Die() {
+
+    }
+}
---
+++ ProjectFMMO-master/Assets/Scripts/PlayerInBattle.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Actions {
+    JUMP,
+    HAMMER,
+    ITEMS,
+    RUN
+}
+
+public class PlayerInBattle : MonoBehaviour {
+    public int HP; //find a way to keep this stored in a more 
+                   //general player class, then bring their hitpoints here    
+
+    List<GameObject> validEnemies;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void DoAction(int actionChosen) {
+        if (actionChosen == (int)Actions.JUMP) {
+            //if any valid targets available (not too often targets will be physically impossible to jump on):
+            //pick a target
+            validEnemies = ListAllEnemies((int)Actions.JUMP);
+            if (validEnemies.Count > 0) {
+                print("jump on something");
+            }
+            else {
+                print("no valid targets!");
+            }
+        }
+        if (actionChosen == (int)Actions.HAMMER) {
+            //if any valid targets available:
+            //pick a target
+            validEnemies = ListAllEnemies((int)Actions.HAMMER);
+            if (validEnemies.Count > 0) {
+                print("hammer something");
+            }
+            else {
+                print("no valid targets!");
+            }
+        }
+        if (actionChosen == (int)Actions.ITEMS) {
+            //if any items available for use:
+
+        }
+        if (actionChosen == (int)Actions.RUN) {
+            //if NOT in a boss fight:
+
+        }
+    }
+
+    public List<GameObject> ListAllEnemies(int attackChosen) {
+        //
+        GameObject[] activeEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+        List<GameObject> validEnemies = new List<GameObject>();
+
+        if (attackChosen == (int)Actions.HAMMER) {
+            foreach (GameObject e in activeEnemies) {
+                if (!e.GetComponent<Enemy>().notHammerable) {
+                    validEnemies.Add(e);
+                }
+            }
+        }
+        if (attackChosen == (int)Actions.JUMP) {
+            foreach (GameObject e in activeEnemies) {
+                if (!e.GetComponent<Enemy>().notJumpable) {
+                    validEnemies.Add(e);
+                }
+            }
+        }
+
+        return validEnemies;
+    }
+
+    public void DamageIncoming() {
+
+    }
+}
