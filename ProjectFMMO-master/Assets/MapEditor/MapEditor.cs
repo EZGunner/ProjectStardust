5e9808fc03271d3000e65250b08a554a7dcdc956
levelengine-git <62917351+levelengine-git@users.noreply.github.com>
2020-04-10 20:10:17 -0400

    started work on a map editor

---
+++ ProjectFMMO-master/Assets/MapEditor/MapEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditor : MonoBehaviour {
+
+    public GameObject selectedTile; //tile player has currently chosen
+
+
+    
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mousePositionSnap = new Vector3
+                (RoundToWholeNum(mousePosition.x), 
+                RoundToWholeNum(mousePosition.y),
+                0);
+
+            Instantiate(selectedTile, 
+                new Vector3(mousePositionSnap.x, mousePositionSnap.y, 10f), 
+                Quaternion.identity);
+        }
+    }
+
+    float RoundToWholeNum(float value) {
+        if (value - (int)value >= 0.5) {
+            return Mathf.Ceil(value);
+        }
+        else {
+            return Mathf.Floor(value);
+        }
+    }
+}
