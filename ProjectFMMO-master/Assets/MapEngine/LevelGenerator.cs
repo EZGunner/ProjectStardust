b165a2460858a6bbabe72e4873d92ccc0f77bf74
IsaacIngram22 <iingram@students.casciahall.com>
2020-04-14 20:21:43 -0500

    Create 2D Map Editor
    Create a 2D map editor which can be used to generate maps from a drawing. Although it's cool in concept, it doesn't actually work in this commit.

---
+++ ProjectFMMO-master/Assets/MapEngine/LevelGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    
+    public Texture2D map;
+
+    public ColorToPrefab[] colorMappings;
+
+    void Start()
+    {
+        generateLevel();
+    }
+
+    private void generateLevel() {
+        for(int x = 0; x < map.width; x++) {
+            for(int y = 0; y < map.height; y++) {
+                generateTile(x, y);
+            }
+        }
+    }
+
+    private void generateTile(int x, int y) {
+        Color pixelColor = map.GetPixel(x, y);
+
+        //Debug.Log("Checking new pixel at (" + x + "," + y + ").");
+
+        if(pixelColor.a == 0) {
+            // The pixel is transparent, so we should ignore it
+            return;
+            //Debug.Log("Above pixel is transparent");
+        }
+
+        Debug.Log("Above pixel is NOT transparent");
+
+        foreach (ColorToPrefab colorMapping in colorMappings) {
+            if(colorMapping.color == pixelColor) {
+                Vector2 position = new Vector2(x, y);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity);
+                //Debug.Log("New Pixel: "+pixelColor);
+            }
+        }
+
+    }
+
+}
